d09cebd0b452dbdea95d8c807f4f734d173edd05
cdwithaks <akshatmadhani@gmail.com>
2026-04-16 18:29:20 +0530

    made some changes

--- frontend/backend/routes/suggestion.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getSuggestions } from '../controller/suggestion.js';
-import auth from '../middleware/auth.js'
-const router = express.Router();
-
-router.post('/', auth, getSuggestions);
-
-export default router;
